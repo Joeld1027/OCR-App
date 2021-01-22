fa90c55a8b64493157c8c2e66bbee2009538f000
aruki <Joeld1027@gmail.com>
2021-01-21 20:50:04 -0600

    added  routes

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const worker = createWorker({
 });
 
  const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
+  destination: (req, file, cb) => {
     cb(null, './uploads');
   },
-  filename: (req, res, cb) =>{
-    cb(null, req.file);
+  filename: (req, file, cb) =>{
+    cb(null, file.originalname);
   }
  });
 
@@ -20,8 +20,24 @@ const worker = createWorker({
 
  app.set('view engine', 'ejs');
 
- app.get('/upload', (req, res)=>{
+ app.get('/', (req,res)=>{
+   res.render('index.ejs')
+ })
+ app.post('/upload', (req, res)=>{
+  upload(req, res, err => {
+    fs.readFile(`${req.file.originalname}`, (err,data) => {
+      if(err) return console.log('this is your error', err)
 
+      worker.recognize(
+  'https://tesseract.projectnaptha.com/img/eng_bw.png',
+  'eng',
+  { logger: m => console.log(m) }
+).then(({ data: { text } }) => {
+  console.log(text);
+})
+    })
+  })
+  res.redirect('back')
  })
 
  app.listen(5000 || process.env.PORT, () => console.log('Running on port 5000'))
