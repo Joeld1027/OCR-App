5fc67e0ab29e469d4be33fe02060eca54470a1bd
aruki <Joeld1027@gmail.com>
2021-01-22 09:28:35 -0600

    added css file and link to html

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const upload = multer({
 }).single('avatar');
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
