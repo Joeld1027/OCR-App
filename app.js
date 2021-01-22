7ff9c20d191e6415b5fef6f395de50d5f1ea8c6b
aruki <Joeld1027@gmail.com>
2021-01-22 08:46:40 -0600

    set the tesseract to properly read files

--- app.js
+++ app.js
@@ -2,42 +2,51 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const multer = require('multer');
-const { createWorker } = require('tesseract.js');
+const {
+  createWorker
+} = require('tesseract.js');
 const worker = createWorker({
   logger: m => console.log(m)
 });
 
- const storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads');
   },
-  filename: (req, file, cb) =>{
+  filename: (req, file, cb) => {
     cb(null, file.originalname);
   }
- });
+});
 
- const upload = multer({storage: storage}).single('avatar');
+const upload = multer({
+  storage: storage
+}).single('avatar');
 
- app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
- app.get('/', (req,res)=>{
-   res.render('index.ejs')
- })
- app.post('/upload', (req, res)=>{
-  upload(req, res, err => {
-    fs.readFile(`${req.file.originalname}`, (err,data) => {
-      if(err) return console.log('this is your error', err)
-
-      worker.recognize(
-  'https://tesseract.projectnaptha.com/img/eng_bw.png',
-  'eng',
-  { logger: m => console.log(m) }
-).then(({ data: { text } }) => {
-  console.log(text);
+app.get('/', (req, res) => {
+  res.render('index.ejs')
 })
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+      if (err) return console.log('this is your error', err);
+      (async () => {
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const {
+          data: {
+            text
+          }
+        } = await worker.recognize('https://www.bibleverseimages.com/love-bible-verse-2l.jpg');
+        console.log(text);
+        await worker.terminate();
+      })();
+
     })
   })
   res.redirect('back')
- })
+})
 
- app.listen(5000 || process.env.PORT, () => console.log('Running on port 5000'))
+app.listen(5000 || process.env.PORT, () => console.log('Running on port 5000'))
